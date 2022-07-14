89c8be7fdc5977867251d7754de56d797c3e4bba
Sergey Lychagin <99026818+sergpythonist@users.noreply.github.com>
2022-07-14 19:18:36 +0300

    Add constructor to class SheetManager

---
+++ sheet_manager.py
@@ -0,0 +1,21 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+class SheetManager:
+
+    def __init__(self):
+        self.options = webdriver.ChromeOptions()
+        self.options.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.options)
+
+
+
+
+if __name__ == "__main__":
+    sheet_creator = SheetManager()
+
