96da52374cec82a01015fc9c5bf27c1bb13382a0
Sergey Lychagin <99026818+sergpythonist@users.noreply.github.com>
2022-07-14 18:02:49 +0300

    Add input_data file

--- data_manager.py
+++ data_manager.py
@@ -1,16 +1,8 @@
 import json
-from urllib.parse import parse_qs, urlparse
 import requests
+from urllib.parse import parse_qs, urlparse
 from bs4 import BeautifulSoup
-
-
-GOOGLE_FORM_LINK = "https://docs.google.com/forms/d/e/1FAIpQLSdZeaipy_AvGN2RgXRysuKObsS_g0l7QJJCshCn3nWR5JlQ0w" \
-                   "/viewform?usp=sf_link "
-HEADERS = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
-    "accept-language": "ru-RU"
-}
-FIRST_PAGE_URL = "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.56276167822266%2C%22east%22%3A-122.30389632177734%2C%22south%22%3A37.69261345230467%2C%22north%22%3A37.857877098316834%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D"
+from input_data import FIRST_PAGE_URL, HEADERS, change_url
 
 
 class DataManager:
@@ -36,8 +28,7 @@ class DataManager:
     def get_data(self):
         # for page in range(1, self.get_page_count() + 1)
         for page in range(1, 3):
-            url = "https://" + f'www.zillow.com/homes/for_rent/1-_beds/{page}_p/?searchQueryState=%7B%22pagination%22%3A%7B%22currentPage%22%3A{page}%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-122.63417281103516%2C%22east%22%3A-122.23248518896484%2C%22south%22%3A37.662044042279824%2C%22north%22%3A37.88836565815623%7D%2C%22mapZoom%22%3A12%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D'
-            response = requests.get(url, headers=HEADERS).text
+            response = requests.get(change_url(page), headers=HEADERS).text
             soup = BeautifulSoup(response, "html.parser")
             self.data.append(json.loads(soup.select_one("script[data-zrr-shared-data-key]").contents[0].strip("!<>-")))
 
@@ -61,11 +52,10 @@ class DataManager:
                     self.prices.append(int(result["price"].strip("$+/mo").replace(",", "")))
 
 
-
 if __name__ == "__main__":
     data_parser = DataManager()
     data_parser.get_data()
     data_parser.get_house_links()
     data_parser.get_addresses()
     data_parser.get_prices()
-
+    print(data_parser.prices)
