47c6e5762d873babfdb92aa445222c132584deae
Sergey Lychagin <99026818+sergpythonist@users.noreply.github.com>
2022-07-14 19:59:33 +0300

    Add fill_sheet function

--- sheet_manager.py
+++ sheet_manager.py
@@ -1,9 +1,11 @@
-import time
+from time import sleep
+import data_manager
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from input_data import GOOGLE_FORM_LINK
 
 
 class SheetManager:
@@ -13,9 +15,22 @@ class SheetManager:
         self.options.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.options)
 
+    def fill_sheet(self):
+        self.driver.get(GOOGLE_FORM_LINK)
+        sleep(3)
+        enter_address = self.driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+        enter_price = self.driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+        enter_link = self.driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+        enter_send_button = self.driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
+
+        enter_address.send_keys("fsdfs")
+        enter_price.send_keys("fsdf")
+        enter_link.send_keys("fdsdf")
+        enter_send_button.send_keys(Keys.ENTER)
 
 
 
 if __name__ == "__main__":
     sheet_creator = SheetManager()
+    sheet_creator.fill_sheet()
 
