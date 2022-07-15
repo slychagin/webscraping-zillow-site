17c30bf4b3d242c0f8f85b31204db4390597987c
Sergey Lychagin <99026818+sergpythonist@users.noreply.github.com>
2022-07-15 10:27:57 +0300

    Add ability saving to csv file

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from data_manager import DataManager
-from sheet_manager import SheetManager
+from sheet_manager import SheetManager, save_to_csv
 
 parser = DataManager()
 
-input_page_count = int(input(f"Zillow ran {parser.get_page_count()} pages with 40 rental ads per page."
+input_page_count = int(input(f"Zillow ran {parser.get_page_count()} pages with 40 rental ads per page "
                              f"How many pages to save in a file? "))
+save_method = input("Choose how to save a data. Enter 'google' to save to Google sheet, 'csv' - to csv file ").lower()
+
 parser.get_data(input_page_count)
 parser.get_addresses()
 parser.get_prices()
 parser.get_house_links()
 total_list = list(zip(parser.addresses, parser.prices, parser.house_links))
-print(list(total_list))
 
-
-sheet_creator = SheetManager()
-
-for (address, price, link) in total_list:
-    sheet_creator.fill_sheet(address, price, link)
+if save_method == "google":
+    sheet_creator = SheetManager()
+    for (address, price, link) in total_list:
+        sheet_creator.fill_google_sheet(address, price, link)
+else:
+    save_to_csv(total_list)
--- sheet_manager.py
+++ sheet_manager.py
@@ -1,3 +1,4 @@
+import csv
 from time import sleep
 from data_manager import DataManager
 from selenium import webdriver
@@ -16,7 +17,7 @@ class SheetManager:
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.options)
         self.data_manager = DataManager()
 
-    def fill_sheet(self, address, price, link):
+    def fill_google_sheet(self, address, price, link):
         self.driver.get(GOOGLE_FORM_LINK)
         sleep(1)
 
@@ -35,3 +36,12 @@ class SheetManager:
 
         enter_next_answer = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[1]/div/div[4]/a')
         enter_next_answer.send_keys(Keys.ENTER)
+        self.driver.quit()
+
+
+def save_to_csv(data):
+    with open("rent.csv", "w", newline='') as file:
+        writer = csv.writer(file, delimiter=";")
+        writer.writerow(["Address", "Price per month", "Link to the property?"])
+        writer.writerows(data)
+
